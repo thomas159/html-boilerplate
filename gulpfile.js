a1924913fa3ff85fedb4adac4e79017795e97734
thomas159 <thomas159@gmail.com>
2017-01-19 13:14:12 +0100

    remove jshint

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,6 @@ var gulp = require('gulp'),
     sass = require('gulp-ruby-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     cssnano = require('gulp-cssnano'),
-    jshint = require('gulp-jshint'),
     uglify = require('gulp-uglify'),
     imagemin = require('gulp-imagemin'),
     rename = require('gulp-rename'),
@@ -35,8 +34,6 @@ gulp.task('styles', function() {
 // Scripts
 gulp.task('scripts', function() {
     return gulp.src('src/scripts/**/*.js')
-        // .pipe(jshint('.jshintrc'))
-        // .pipe(jshint.reporter('default'))
         .pipe(concat('main.js'))
         .pipe(gulp.dest('dist/scripts'))
         .pipe(rename({ suffix: '.min' }))
